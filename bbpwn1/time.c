a81abd7be524f2bd90d6849b7f5716bb2b38ba0c
Amadeus8182 <amadeus8182@gmail.com>
2023-11-04 11:41:31 -0500

    bbpwn1 solve

--- bbpwn1/time.c
+++ bbpwn1/time.c
@@ -2,33 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void pwn() {
-    srand(time(NULL));
-    
-    char s[20];
-    long long randomNumber;
-    long long inputNumber;
-    
-    randomNumber = ((long long)rand() << 32) | rand();
-	printf("%lld", randomNumber);
-    fgets(s, 50, stdin);
-    inputNumber = strtoll(s, NULL, 10);
-    
-    if (randomNumber == inputNumber) {
-        printf("Congrats! The correct number was %lld!\n", randomNumber);
-        system("cat flag.txt");
-    } else {
-        printf("Sorry, the correct number was %lld, not %lld.\n", randomNumber, inputNumber);
-        printf("Better luck next time!\n");
-    }
-}
-
 int main() {
     setvbuf(stdin, NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
 
-    printf("Guess a number: ");
-    pwn();
-
+	time_t t = time(NULL);
+	srand(1699115700);
+	printf("%d\n" t);
+	printf("%lld", ((long long)rand() << 32) | rand());
     return 0;
 }
